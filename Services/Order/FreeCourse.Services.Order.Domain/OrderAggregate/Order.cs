0ce809bc5b3689a45358ae50176a79aa60f42b79
anti-pc <buketculfaoglu@gmail.com>
2024-02-13 03:58:22 +0300

    Order api base structure

---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
@@ -0,0 +1,38 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class Order : Entity, IAggregateRoot
+    {
+        public DateTime CreatedDate { get; private set; }
+        public Address Address { get; private set; }
+        public string BuyerId { get; private set; }
+
+        private readonly List<OrderItem> _orderItems;
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+        //public IReadOnlyCollection<OrderItem> OrderItems { get { return _orderItems; } }
+
+
+        public Order(string buyerId, Address address)
+        {
+            Address = address;
+            BuyerId = buyerId;
+            CreatedDate = DateTime.Now;
+            _orderItems = new List<OrderItem>();
+        }
+
+        public void AddOrderItem(string productId, string productName, string pictureUrl, decimal price)
+        {
+            var existProduct = _orderItems.Any(x => x.ProductId == productId);
+
+            if (!existProduct)
+            {
+                var newOrderItem = new OrderItem(productId, productName, pictureUrl, price);
+                _orderItems.Add(newOrderItem);
+            }
+        }
+
+        public decimal GetTotalPrice => _orderItems.Sum(x => x.Price);
+
+    }
+}
