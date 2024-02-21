a2d8d31013c96e631c86c7516c7f03173b288a2b
anti-pc <buketculfaoglu@gmail.com>
2024-02-22 00:44:23 +0300

    Create Course page and actions nullable disabled in the .csproj file to allow nullable strings

--- Frontends/FreeCourse.Web/Controllers/CourseController.cs
+++ Frontends/FreeCourse.Web/Controllers/CourseController.cs
@@ -1,7 +1,9 @@
 ﻿using FreeCourse.Shared.Services;
+using FreeCourse.Web.Models.Catalog;
 using FreeCourse.Web.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FreeCourse.Web.Controllers
 {
@@ -21,5 +23,33 @@ namespace FreeCourse.Web.Controllers
         {
             return View(await _catalogService.GetAllCourseByUserIdAsync(_sharedIdentityService.GetUserId));
         }
+
+        public async Task<IActionResult> Create()
+        {
+            var categories = await _catalogService.GetAllCategoryAsync();
+            ViewBag.categoryList = new SelectList(categories, "Id", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CourseCreateInput courseCreateInput)
+        {
+            var categories = await _catalogService.GetAllCategoryAsync();
+            ViewBag.categoryList = new SelectList(categories, "Id", "Name");
+            
+            if (!ModelState.IsValid)
+                return View();
+
+            courseCreateInput.UserId = _sharedIdentityService.GetUserId;
+
+
+            await _catalogService.CreateCourseAsync(courseCreateInput);
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
     }
 }
--- Frontends/FreeCourse.Web/Models/Catalog/FeatureViewModel.cs
+++ Frontends/FreeCourse.Web/Models/Catalog/FeatureViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace FreeCourse.Web.Models.Catalog
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Web.Models.Catalog
 {
     public class FeatureViewModel
     {
+        [Required]
+        [Display(Name = "Course Duration")]
         public int Duration { get; set; }
     }
 }
