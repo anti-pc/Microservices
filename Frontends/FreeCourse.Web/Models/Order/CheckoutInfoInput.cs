b88e0f1dacd9c80c0d6a77b3b2a6b0f650d3c024
anti-pc <buketculfaoglu@gmail.com>
2024-03-07 17:08:56 +0300

    OrderService implementation (sync)

---
+++ Frontends/FreeCourse.Web/Models/Order/CheckoutInfoInput.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Web.Models.Order
+{
+    public class CheckoutInfoInput
+    {
+        [Display(Name="City")]
+        public string Province { get; set; }
+
+        public string District { get; set; }
+
+        public string Street { get; set; }
+
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "Address")]
+        public string Line { get; set; }
+
+
+        [Display(Name = "Firstname Lastname")]
+        public string CardName { get; set; }
+
+        [Display(Name = "Card Number")]
+        public string CardNumber { get; set; }
+
+        [Display(Name = "Expiration Date (month/year)")]
+        public string Expiration { get; set; }
+
+        [Display(Name = "CVV / CVC2")]
+        public string CVV { get; set; }
+
+        [Display(Name = "Total Price")]
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ Frontends/FreeCourse.Web/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using FreeCourse.Web.Models.Order;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<OrderCreatedViewModel> CreateOrder(CheckoutInfoInput checkoutInfoInput);
+
+        //Async with RabbitMQ
+        Task SuspendOrder(CheckoutInfoInput checkoutInfoInput);
+
+        Task<List<OrderViewModel>> GetOrder();
+    }
+}
